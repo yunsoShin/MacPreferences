cf340b9745d3722964f8da711e9f9b883c6d3646
Yunsu <shin4895@gmail.com>
2024-06-08 00:03:28 +0900

    안정화버전 , lazy까지 자동화 완성

--- setup_nvim.sh
+++ setup_nvim.sh
@@ -3,24 +3,24 @@
 # 사용자 홈 디렉토리로 이동
 cd ~
 
+# 기존 nvim 설정 삭제
+rm -rf .config/nvim
+
 # .config/nvim 디렉토리 생성
 mkdir -p .config/nvim
-cd .config/nvim
 
 # lua 디렉토리 및 하위 디렉토리 생성
-mkdir -p lua/config lua/plugins lua/utils
+mkdir -p .config/nvim/lua/config .config/nvim/lua/plugins .config/nvim/lua/utils
 
-# lua/config/init.lua 파일 생성
-touch lua/config/init.lua
+# nvim/init.lua 파일 생성 및 초기화
+touch .config/nvim/init.lua
 
-# nvim을 이용해 lua/config/init.lua 파일을 열고 require("config") 추가
-nvim -c 'normal ggOrequire("config")' -c 'wq' lua/config/init.lua || true
+# nvim을 이용해 init.lua 파일을 열고 require("config") 추가
+nvim -c 'normal ggOrequire("config")' -c 'wq' .config/nvim/init.lua || true
 
-# 에러에서 벗어나도록 엔터 키 입력
-echo -e "\n\n" | read -p "Press Enter to continue..."
 
 # lua/config/init.lua 파일에 추가 설정 작성
-cat <<EOL >> lua/config/init.lua
+cat <<EOL > .config/nvim/lua/config/init.lua
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   vim.fn.system({
@@ -40,5 +40,3 @@ local opts = {}
 require("lazy").setup(plugins, opts)
 EOL
 
-# nvim을 실행하고 :Lazy 명령어 실행
-nvim -c 'Lazy'
