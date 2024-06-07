677ec7d8a577b666bafcf90777af655f0de0aa19
Yunsu <shin4895@gmail.com>
2024-06-08 00:54:33 +0900

    색상 플러그인 적용

--- setup_nvim.sh
+++ setup_nvim.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 
-# 사용자 홈 디렉토리로 이동
-cd ~
+echo "사용자 홈 디렉토리로 이동..."
+cd ~ || { echo "홈 디렉토리로 이동 실패"; exit 1; }
 
-# 기존 nvim 설정 삭제
-rm -rf .config/nvim
+echo "기존 nvim 설정 삭제..."
+rm -rf .config/nvim || { echo "기존 nvim 설정 삭제 실패"; exit 1; }
 
-# .config/nvim 디렉토리 생성
-mkdir -p .config/nvim
+echo ".config/nvim 디렉토리 생성..."
+mkdir -p .config/nvim || { echo ".config/nvim 디렉토리 생성 실패"; exit 1; }
 
-# lua 디렉토리 및 하위 디렉토리 생성
-mkdir -p .config/nvim/lua/config .config/nvim/lua/plugins .config/nvim/lua/utils
+echo "lua 디렉토리 및 하위 디렉토리 생성..."
+mkdir -p .config/nvim/lua/config .config/nvim/lua/plugins .config/nvim/lua/utils || { echo "lua 디렉토리 및 하위 디렉토리 생성 실패"; exit 1; }
 
-# nvim/init.lua 파일 생성 및 초기화
-touch .config/nvim/init.lua
+echo "nvim/init.lua 파일 생성 및 초기화..."
+touch .config/nvim/init.lua || { echo "nvim/init.lua 파일 생성 실패"; exit 1; }
 
-# nvim을 이용해 init.lua 파일을 열고 require("config") 추가
-nvim -c 'normal ggOrequire("config")' -c 'wq' .config/nvim/init.lua || true
+echo "nvim을 이용해 init.lua 파일을 열고 require('config') 추가..."
+nvim -c 'normal ggOrequire("config")' -c 'wq' .config/nvim/init.lua || { echo "init.lua 파일 편집 실패"; exit 1; }
 
-
-# lua/config/init.lua 파일에 추가 설정 작성
+echo "lua/config/init.lua 파일에 추가 설정 작성..."
 cat <<EOL > .config/nvim/lua/config/init.lua
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -39,4 +38,37 @@ local opts = {}
 
 require("lazy").setup(plugins, opts)
 EOL
+echo "lua/config/init.lua 파일 작성 완료."
 
+# .lazy_check 파일 초기화
+echo "" > ~/.lazy_check
+
+echo "nvim을 이용해 :Lazy 명령어 실행..."
+nvim -c 'Lazy' -c 'w ~/.lazy_check' -c 'q' || { echo "nvim :Lazy 명령어 실행 실패"; exit 1; }
+
+# .lazy_check 파일 확인
+if grep -q 'Loaded plugins' ~/.lazy_check; then
+  echo "success"
+else
+  echo "Lazy.nvim 설치 검증 실패"
+fi
+
+# .lazy_check 파일 삭제
+rm ~/.lazy_check
+
+echo "plugins/gruvbox.lua 파일 생성 및 설정 추가..."
+cat <<EOL > ~/.config/nvim/lua/plugins/gruvbox.lua
+return {
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
+    lazy = false,
+    config = function()
+        vim.cmd([[colorscheme gruvbox]])
+    end
+}
+EOL
+
+# init.lua 파일의 local plugins 객체 갱신
+sed -i '' 's/local plugins = {}/local plugins = "plugins"/' ~/.config/nvim/lua/config/init.lua
+
+echo "스크립트 실행 완료."
